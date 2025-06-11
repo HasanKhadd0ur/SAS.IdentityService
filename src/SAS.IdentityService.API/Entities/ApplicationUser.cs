fe5d9b94c2c6539884bed7bc9f9108c51b661bf3
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-06-11 19:15:49 +0300

    Add Entities

--- src/SAS.IdentityService.API/Entities/ApplicationUser.cs
+++ src/SAS.IdentityService.API/Entities/ApplicationUser.cs
@@ -4,4 +4,6 @@ namespace SAS.IdentityService.API.Entities;
 public class ApplicationUser : IdentityUser<Guid>
 {
     public String FirstName { get; set; }
+
+    public String LastName { get; set; }
 }
