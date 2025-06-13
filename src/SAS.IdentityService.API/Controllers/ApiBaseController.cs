29ca10f50fca876c2340bd7a6da2b05d5c669ec6
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-06-13 12:02:29 +0300

    Add Controllers

---
+++ src/SAS.IdentityService.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,28 @@
+﻿using Ardalis.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SAS.IdentityService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+
+        protected IActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+            {
+                return Ok(result.Value);
+            }
+            else
+            {
+                return Problem(
+                    detail: result.ValidationErrors.FirstOrDefault().ErrorMessage,
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: result.ValidationErrors.FirstOrDefault().ErrorCode
+                    );
+            }
+        }
+
+    }
+}
