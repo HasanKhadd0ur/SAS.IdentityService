d56cee1509264efcfa78a527759a7cc371ab09aa
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-01 10:11:08 +0300

    Regsister Services

--- src/SAS.IdentityService.API/Program.cs
+++ src/SAS.IdentityService.API/Program.cs
@@ -1,89 +1,13 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using SAS.IdentityService.API.Abstraction;
-using SAS.IdentityService.API.Data;
-using SAS.IdentityService.API.Entities;
-using SAS.IdentityService.API.Services;
-using System.Text;
+using SAS.EventsService.Infrastructure.Persistence.DependencyInjection;
+using SAS.EventsService.Infrastructure.Services.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
 
 
-// Load JWT settings
-var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
-builder
-    .Services.Configure<JwtSetting>(jwtSettingsSection);
-var jwtSettings = jwtSettingsSection.Get<JwtSetting>();
-
-// Add DbContext
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Add Identity
-builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
-{
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = true;
-})
-.AddEntityFrameworkStores<AppDbContext>()
-.AddDefaultTokenProviders();
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.MinimumSameSitePolicy = SameSiteMode.Lax;
-});
-
-
-builder.Services.AddAuthentication(options =>
-{
-    // Set JWT as the default for APIs
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-})
-.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-{
-    options.RequireHttpsMetadata = true;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = jwtSettings.Issuer,
-        ValidateAudience = true,
-        ValidAudience = jwtSettings.Audience,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret)),
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.Zero
-    };
-})
-.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-{
-    options.LoginPath = "/api/auth/login";
-    options.AccessDeniedPath = "/api/auth/access-denied";
-    options.SlidingExpiration = true;
-})
-.AddGoogle("Google", options =>
-{
-    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-    options.CallbackPath = "/api/auth/signin-google";
-    options.SaveTokens = true;
-});
-
-// Add services
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-builder.Services.AddScoped<IUserRoleService, UserRoleService>();
-builder.Services.AddScoped<IUserService, UserService>();
-
+builder.Services.AddPersistence(configuration);
+builder.Services.AddInfrastructureSevices(configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
