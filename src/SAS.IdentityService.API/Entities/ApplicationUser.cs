2a58a466b06beea2faf9c3221d4c7c2f6a35331b
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-01 10:05:10 +0300

    Refactor code to be clean

--- src/SAS.IdentityService.API/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace SAS.IdentityService.API.Entities;
-public class ApplicationUser : IdentityUser<Guid>
-{
-    public String FirstName { get; set; }
-
-    public String LastName { get; set; }
-}
--- src/SAS.IdentityService.API/Models/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SAS.IdentityService.API.Models;
-
-public class LoginRequest
-{
-    public string UserName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
