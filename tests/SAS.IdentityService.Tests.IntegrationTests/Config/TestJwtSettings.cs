92d4d380035ac8aa1547bdf0801d5933f99b3c3f
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-01 22:24:45 +0300

    Add Tests Config

---
+++ tests/SAS.IdentityService.Tests.IntegrationTests/Config/TestJwtSettings.cs
@@ -0,0 +1,20 @@
+﻿using SAS.IdentityService.Infrastructure.Services.Tokens;
+
+namespace SAS.IdentityService.Tests.IntegrationTests.Config
+{
+    public static class TestJwtSettings
+        {
+            public static JwtSetting Get()
+            {
+                return new JwtSetting
+                {
+                    Secret = "THIS_IS_A_SUPER_SECRET_KEY_1234567890",
+                    Issuer = "TestIssuer",
+                    Audience = "TestAudience",
+                    ExpireMinutes = 60
+                };
+            }
+        }
+    
+
+}
