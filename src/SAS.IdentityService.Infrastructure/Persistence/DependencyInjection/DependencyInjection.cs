1a443596f55e99063f1a446e434e45d1f3ce9bb4
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-01 10:12:25 +0300

    Add Persistence

---
+++ src/SAS.IdentityService.Infrastructure/Persistence/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SAS.IdentityService.ApplicationCore.Entities;
+using SAS.IdentityService.Infrastructure.Persistence.DataContext;
+
+
+namespace SAS.EventsService.Infrastructure.Persistence.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDataContext(configuration)
+                .AddRepositories()
+                .AddIdentity();
+
+            return services;
+        }
+
+
+
+
+        #region Register Identity 
+
+        private static IServiceCollection AddIdentity(this IServiceCollection services)
+        {
+
+            // Add Identity
+            services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+
+
+            return services;
+        }
+        #endregion Register Identity 
+
+        #region Register Repositories
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+
+            return services;
+
+        }
+
+
+        #endregion Register Repositoryies
+
+        #region Register Data context 
+        private static IServiceCollection AddDataContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add DbContext
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+          
+            return services;
+
+        }
+
+        #endregion Register Data Context 
+    }
+}
