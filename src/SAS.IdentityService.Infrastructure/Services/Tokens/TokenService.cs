38d53e13d4322e1c6e89739ce774042642b56037
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-19 21:07:40 +0300

    Fix token generatiion

--- src/SAS.IdentityService.Infrastructure/Services/Tokens/TokenService.cs
+++ src/SAS.IdentityService.Infrastructure/Services/Tokens/TokenService.cs
@@ -1,4 +1,5 @@
 ﻿using Ardalis.Result;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using SAS.IdentityService.ApplicationCore.Contracts.Tokens;
@@ -15,6 +16,8 @@ namespace SAS.IdentityService.Infrastructure.Services.Tokens
     {
 
         private readonly JwtSetting _jwtSetting;
+        private readonly UserManager<ApplicationUser> _userManager;
+
         public TokenService(IOptions<JwtSetting> jwtOptions)
         {
             _jwtSetting = jwtOptions.Value;
@@ -22,19 +25,18 @@ namespace SAS.IdentityService.Infrastructure.Services.Tokens
 
         public Task<string> GenerateRefreshTokenAsync(ApplicationUser user)
         {
-            return Task.FromResult(GenerateToken(user));
+            return GenerateToken(user);
         }
 
-        public string GenerateToken(ApplicationUser user)
+        public async Task<string> GenerateToken(ApplicationUser user)
         {
             var rsa = RSA.Create();
             rsa.ImportPkcs8PrivateKey(Convert.FromBase64String(_jwtSetting.PrivateKey), out _);
-            
+
             var signingCredentials = new SigningCredentials(
                 new RsaSecurityKey(rsa),
                 SecurityAlgorithms.RsaSha256);
 
-            
             var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
@@ -42,6 +44,16 @@ namespace SAS.IdentityService.Infrastructure.Services.Tokens
                 new Claim(ClaimTypes.Email, user.Email),
                 new Claim(ClaimTypes.GivenName, user.FirstName)
             };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            if (roles is not null)
+            {
+                foreach (var role in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+            }
 
             var securityToken = new JwtSecurityToken(
                 issuer: _jwtSetting.Issuer,
